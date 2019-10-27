a25c1b76f48537e3e7171112cddbf2b0df30f49c
Leko <leko.noor@gmail.com>
2019-10-27 16:14:33 +0900

    fix pretense path

--- index.jsx
+++ index.jsx
@@ -1,17 +1,18 @@
 const { remote } = require("electron")
 
-exports.middleware = store => next => action => {
-  console.log(action)
-  return next(action)
-}
-
 exports.mapTermsDispatch = (dispatch, map) => ({
   ...map,
-  onDropFile(file) {
-    // dispatch
-    // term.write(file.path)
+  onDropFile(_, uid, file) {
+    map.onData(uid, file.path)
   }
 })
+
+const passProps = (uid, parentProps, props) => ({
+  ...props,
+  onDropFile: (...args) => parentProps.onDropFile(uid, ...args)
+})
+exports.getTermGroupProps = passProps
+exports.getTermProps = passProps
 
 exports.decorateTerm = (Term, { React }) =>
   class extends React.Component {
@@ -20,13 +21,11 @@ exports.decorateTerm = (Term, { React }) =>
     }
 
     handleDrop = e => {
-      const { term } = this.props
       remote.getCurrentWindow().focus()
       this.props.onDropFile(e.dataTransfer.files[0])
     }
 
     render() {
-      console.log(this.props.onDropFile)
       return (
         <div
           style={{ position: "absolute", top: 0, bottom: 0, right: 0, left: 0 }}
